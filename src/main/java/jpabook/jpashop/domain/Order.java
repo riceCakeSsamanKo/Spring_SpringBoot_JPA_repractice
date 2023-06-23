e526a06ece5d46b1ca4cf4d94a89e85a854ea152
ricecake_ssamanko <mulsoap0504@gmail.com>
2023-06-23 13:26:19 +0900

    update

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -35,9 +35,10 @@ public class Order {
     // 꿀팁! OneToOne일때 주인 지정: 많이 사용하는 놈한테 주인설정을 준다.
     private Delivery delivery;
 
-    private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    private LocalDateTime orderDate;
 
     // 연관관계 메소드 (연관관계 편의 메소드의 위치는 핵심적으로 컨트롤하는 쪽)
     public void setMember(Member member) {
@@ -53,5 +54,50 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER); // 주문 상태: ORDER로 초기화
+        order.setOrderDate(LocalDateTime.now());  // 주문 시간 현재
+        return order;
+    }
+
+    //==비즈니스 로직==//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        // 이미 배송 완료된 경우
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            // 배송 취소 불가
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        // 주문 취소
+        setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            // 주문 수량 복구
+            orderItem.cancel();
+        }
+    }
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
