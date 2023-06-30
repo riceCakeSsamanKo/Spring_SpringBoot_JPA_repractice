53e6696253292bfc850093a512cbf081f342b49b
Min Jeon <mulsoap0504@gmail.com>
2023-06-30 14:10:58 +0900

    update

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import jpabook.jpashop.domain.Delivery;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import javax.persistence.*;
 
@@ -17,5 +19,12 @@ import javax.persistence.*;
 public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module(); //강제 지연 로딩 설정
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
 	}
 }
--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,9 +26,25 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * 주의: 엔티티를 직접 노출할 때는 양방향 연관관계가 걸린 곳은 꼭!
+     * 한곳을 @JsonIgnore 처리 해야 한다.
+     * 안그러면 양쪽을 서로 호출하면서 무한 루프가 걸린다.
+     * */
     @GetMapping("api/v1/simple-orders")
-    public List<Order> ordersv1() {
+    public List<Order> ordersV1() {  // 엔티티를 직접 노
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();  // Lazy 강제 초기화. Proxy 객체를 사용해서 정보 땡겨
+            order.getDelivery().getAddress(); //Proxy: Lazy 강제 초기화
+        }
+        return all;
+        // Order에는 member가 있고 Member에는 orders가 있다.
+        // 따라서 Order를 조회해 오면 종속된 member를 가져오고, 또 해당 member 내부에 있는 orders를 또 가져오고 orders의 각 order에 있는 member를 또 가져오고.....
+        // 이런식으로 무한 루프가 발생하게 된다.
 
+        // Member.orders에 @JsonIgnore로 해서 가져온다고 해도, Order.member가 FetchType.LAZY이므로
+        // 가져온 객체의 타입이 불분명해서 또 에러가 발생한다.
+        // (Type definition error: [simple type, class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor];)
     }
 }
