9f041dfee7a6f669a0d3c5cc9caaa01cac62621b
Min Jeon <mulsoap0504@gmail.com>
2023-07-02 10:24:44 +0900

    Update OrderSimpleApiController.java

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,18 +1,18 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Delivery;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -30,7 +30,7 @@ public class OrderSimpleApiController {
      * 주의: 엔티티를 직접 노출할 때는 양방향 연관관계가 걸린 곳은 꼭!
      * 한곳을 @JsonIgnore 처리 해야 한다.
      * 안그러면 양쪽을 서로 호출하면서 무한 루프가 걸린다.
-     * */
+     */
     @GetMapping("api/v1/simple-orders")
     public List<Order> ordersV1() {  // 엔티티를 직접 노
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -46,5 +46,35 @@ public class OrderSimpleApiController {
         // Member.orders에 @JsonIgnore로 해서 가져온다고 해도, Order.member가 FetchType.LAZY이므로
         // 가져온 객체의 타입이 불분명해서 또 에러가 발생한다.
         // (Type definition error: [simple type, class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor];)
+    }
+
+    @GetMapping("api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2() {
+        // N+1 문제
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDTO> result
+                = orders.stream()
+                .map(o -> new SimpleOrderDTO(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    private class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        SimpleOrderDTO(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName(); // LAZY 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();  // LAZY 초기화
+        }
     }
 }
