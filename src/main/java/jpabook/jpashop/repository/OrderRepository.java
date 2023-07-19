d049a3e382f08a883f0165827ed6ff3cf216cbc5
Min Jeon <mulsoap0504@gmail.com>
2023-07-19 15:19:29 +0900

    Update OrderRepository.java

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -19,6 +19,7 @@ import java.util.List;
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
 
     public void save(Order order) {
         em.persist(order);
