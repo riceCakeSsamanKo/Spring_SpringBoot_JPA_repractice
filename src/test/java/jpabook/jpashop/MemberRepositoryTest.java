355c1fed2a3d46b260d25982fc8b712253bbf0e0
Min Jeon <mulsoap0504@gmail.com>
2023-08-01 16:30:03 +0900

    update

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.MemberRepositoryOld;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,15 +12,18 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.Assert.*;
 
 // 실제 테스트가 아닌 테스트 연습을 위해 만든 테스트 케이스
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
-
     @Autowired
-    MemberRepository memberRepository;
+    EntityManager em;
+    @Autowired
+    MemberRepositoryOld memberRepository;
 
     @Test
     @Transactional  // @Transactional 어노테이션이 테스트 케이스에 존재하는 경우 테이스 종료후 롤백이 디폴트
@@ -29,7 +33,8 @@ public class MemberRepositoryTest {
         //given
         Member member = new Member();
         member.setName("memberA");
-
+        em.flush();
+        em.clear();
         //when
         Long savedId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedId);
